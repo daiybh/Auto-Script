2f992930ab24dacc53aef20c38ca7cb1f1a1ea36
Dai Li <d.li@simplylive.tv>
2021-09-30 21:38:11 +0800

    u1

--- UpdateAll.cmd
+++ UpdateAll.cmd
@@ -1,16 +1,28 @@
 
 @echo off
-goto:start
+
+if "%1"=="" (goto:start)
+echo ...%1
+cd %1
+
+git reset --hard
+git pull 
+git reset --hard HEAD
+git submodule update --init --recursive
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
+git submodule foreach git reset --hard HEAD
+
+call exit
+goto:eof
+
 :UpdateFunc
-	for /d   %%f in (%1\*) do (	
-		cd %%f
-		git reset --hard
-		git pull 
-		git reset --hard HEAD
-		git submodule update --init --recursive
-		git submodule foreach git pull origin master
-		cd ..
+	for /d   %%f in (%2\*) do (	
+		start "%%f" %1 %%f	
 	)
 goto:eof
 :start
-call:UpdateFunc .\
+call:UpdateFunc %0 .\
+
+
+
